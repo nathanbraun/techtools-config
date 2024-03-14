b496ecfd6f28183bdb0c324ffd51c87d3861d07e
Nathan Braun <nathanericbraun@gmail.com>
2024-03-14 08:53:21 -0500

    fix: don't use v1 of hop

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -119,7 +119,7 @@ return {{
   {
     'smoka7/hop.nvim',
     -- tag = '*', -- optional but strongly recommended
-    branch = 'v1', -- optional but strongly recommended
+    -- branch = 'v1', -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
       require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
