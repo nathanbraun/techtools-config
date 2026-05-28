113166d53aabefd1f184ceaaae479722cb474597
Nathan Braun <nathanericbraun@gmail.com>
2026-05-28 11:36:34 -0500

    fix: link highlighting

--- private_dot_config/nvim/lua/user/colorscheme.lua
+++ private_dot_config/nvim/lua/user/colorscheme.lua
@@ -6,3 +6,17 @@ catch /^Vim\%((\a\+)\)\=:E185/
   set background=dark
 endtry
 ]]
+
+local function set_markdown_hl()
+  vim.api.nvim_set_hl(0, "@markup.italic", { italic = true })
+  vim.api.nvim_set_hl(0, "@markup.strong", { bold = true })
+  vim.api.nvim_set_hl(0, "@markup.strikethrough", { strikethrough = true })
+  vim.api.nvim_set_hl(0, "@markup.link",       { fg = "#8fbfdc", underline = true })
+  vim.api.nvim_set_hl(0, "@markup.link.label", { fg = "#8fbfdc", underline = true })
+  vim.api.nvim_set_hl(0, "@markup.link.url",   { fg = "#8fbfdc", underline = true })
+end
+
+vim.api.nvim_create_autocmd({ "ColorScheme", "FileType" }, {
+  callback = set_markdown_hl,
+})
+set_markdown_hl()
